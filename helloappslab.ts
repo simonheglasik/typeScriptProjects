50c8e724603a42e8a2d79f96cb475278385ab73c
simon heglasik <simonheglasik@gmail.com>
2020-10-13 17:11:25 +0200

    equal

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,15 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function equals(a,b,c)
+{
+    let equal : number = 0;
+    let numbers : number[] = [a , b ,c]
+    for(let i : number = 0; i < numbers.length ;i++)
+    {
+        for(let o : number = 0; o < numbers.length ; o++)
+        {
+            if(numbers[i] == numbers[o] && i != o)
+                equal++
+        }
+    } 
+    return equal;
+}
+console.log(equals(1,2,1))
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,13 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function equals(a, b, c) {
+    var equal = 0;
+    var numbers = [a, b, c];
+    for (var i = 0; i < numbers.length; i++) {
+        for (var o = 0; o < numbers.length; o++) {
+            if (numbers[i] == numbers[o] && i != o)
+                equal++;
+        }
+    }
+    return equal;
+}
+console.log(equals(1, 2, 1));
 //# sourceMappingURL=helloappslab.js.map
