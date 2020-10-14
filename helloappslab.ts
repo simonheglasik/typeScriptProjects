a34b7e3231e07b7c7d7c4d331c0a6a691f5ed008
simon heglasik <simonheglasik@gmail.com>
2020-10-14 15:45:49 +0200

    palindrome

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,13 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function isPalindrome(word : String)
+{
+    let palindrome = true;
+    for(let i = 0; i < word.length ; i++)
+    {
+        if(word[i] != word[word.length - 1 - i])
+        {
+            palindrome = false
+        }
+    }
+    return palindrome;
+}
+console.log(isPalindrome("oko"));
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,11 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function isPalindrome(word) {
+    var palindrome = true;
+    for (var i = 0; i < word.length; i++) {
+        if (word[i] != word[word.length - 1 - i]) {
+            palindrome = false;
+        }
+    }
+    return palindrome;
+}
+console.log(isPalindrome("oko"));
 //# sourceMappingURL=helloappslab.js.map
