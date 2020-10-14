b1ce8b54febdafbccec6150b602b65cb8c32850f
simon heglasik <simonheglasik@gmail.com>
2020-10-14 15:08:26 +0200

    multipleByLenght

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,9 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function multipleByLength(a : number[])
+{
+    for(let i = 0; i < a.length;i++)
+    {
+        a[i]*=a.length
+    }
+    return a;
+}
+console.log(multipleByLength([2,1,3]))
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,8 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function multipleByLength(a) {
+    for (var i = 0; i < a.length; i++) {
+        a[i] *= a.length;
+    }
+    return a;
+}
+console.log(multipleByLength([2, 1, 3]));
 //# sourceMappingURL=helloappslab.js.map
