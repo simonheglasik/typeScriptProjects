2aa2d4a05399d69507560fea5814852c69241e96
simon heglasik <simonheglasik@gmail.com>
2020-10-14 10:48:00 +0200

    triangle

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,9 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function isTriangle(a,b,c)
+{
+    if(a + b > c && b + c > a && c + a > b)
+        return true
+
+    else
+        return false
+}
+console.log(isTriangle(2,3,4))
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,8 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function isTriangle(a, b, c) {
+    if (a + b > c && b + c > a && c + a > b)
+        return true;
+    else
+        return false;
+}
+console.log(isTriangle(2, 3, 4));
 //# sourceMappingURL=helloappslab.js.map
