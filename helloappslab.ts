fccf04979926c05b6121b35bb014c29cdfc13afd
simon heglasik <simonheglasik@gmail.com>
2020-10-17 16:39:51 +0200

    rockPaperScissors

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,20 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function rockPaperScissors(playerOne : string,playerTwo : string ) : string
+{
+    let one;
+    let two;
+    let tools : string[] = ["rock" , "paper", "scissors"]
+    if(playerOne == playerTwo)
+        return "Tie"
+    for(let i = 0;i < tools.length; i++)
+    {
+        if(tools[i] == playerOne)
+            one = i;
+        if(tools[i] == playerTwo)
+            two = i;
+    }
+    if(one == two - 1 || one == two + 2)
+        return "Player 2 won"
+    else
+        return "Player 1 won"
+}
+console.log(rockPaperScissors("scissors","rock"));
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,19 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function rockPaperScissors(playerOne, playerTwo) {
+    var one;
+    var two;
+    var tools = ["rock", "paper", "scissors"];
+    if (playerOne == playerTwo)
+        return "Tie";
+    for (var i = 0; i < tools.length; i++) {
+        if (tools[i] == playerOne)
+            one = i;
+        if (tools[i] == playerTwo)
+            two = i;
+    }
+    if (one == two - 1 || one == two + 2)
+        return "Player 2 won";
+    else
+        return "Player 1 won";
+}
+console.log(rockPaperScissors("scissors", "rock"));
 //# sourceMappingURL=helloappslab.js.map
