20ba2152f2cb3dec397e665776bac7faf9e8fa22
simon heglasik <simonheglasik@gmail.com>
2020-10-17 18:58:49 +0200

    minMaxValue

--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,3 +1,15 @@
-var hi = ("Hello appslab");
-console.log(hi);
+function minMaxDifference(numbers) {
+    var min = Number.MAX_VALUE;
+    var max = Number.MIN_VALUE;
+    for (var i = 0; i < numbers.length; i++) {
+        if (numbers[i] < min)
+            min = numbers[i];
+        if (numbers[i] > max)
+            max = numbers[i];
+    }
+    console.log(min);
+    console.log(max);
+    return max - min;
+}
+console.log(minMaxDifference([10, 2, 3, 5, 9, -4, -5]));
 //# sourceMappingURL=helloappslab.js.map
