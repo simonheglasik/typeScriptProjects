74387f1073148372cd5c4b99bb783d6624777ec1
simon heglasik <simonheglasik@gmail.com>
2020-10-17 19:00:27 +0200

    wrong branch

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,17 +1,2 @@
-function minMaxDifference(numbers : number[]) : number
-{
-    let min = Number.MAX_VALUE;
-    let max = Number.MIN_VALUE;
-    for(let i = 0; i < numbers.length; i++)
-    {
-        if(numbers[i] < min)
-            min = numbers[i]
-        if(numbers[i] > max)
-            max = numbers[i]
-    }
-    console.log(min)
-    console.log(max)
-    return max - min
-
-}
-console.log(minMaxDifference([10,2,3,5,9,-4,-5]));
+let hi : String = ("Hello appslab") 
+console.log(hi);
