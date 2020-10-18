5f0348f3aa869cd417013ba8b27bdf1281d58c17
simon heglasik <simonheglasik@gmail.com>
2020-10-18 12:12:46 +0200

    warOfNumbers

--- helloappslab.ts
+++ helloappslab.ts
@@ -1,2 +1,22 @@
-let hi : String = ("Hello appslab") 
-console.log(hi);
+function numbersWar(numbers : number[])
+{
+    let oddScore = 0
+    let evenScore = 0
+    for(let i : number = 0; i< numbers.length;i++)
+    {
+        if(numbers[i] % 2 == 0)
+            evenScore += numbers[i];
+        else
+            oddScore += numbers[i]
+    }
+    console.log("score of odd number is " + oddScore)
+    console.log("score of even number is " + evenScore)
+    if(evenScore > oddScore)
+        return "even numbers won"
+    else if(oddScore > evenScore)
+        return "odd numbers won"
+    else
+        return "strength of both sides is same"
+    
+}
+console.log(numbersWar([5,1,2,5,7,8,4,7,9,1,2,4,7,8,6,4]))
--- out/helloappslab.js
+++ out/helloappslab.js
@@ -1,15 +1,20 @@
-function minMaxDifference(numbers) {
-    var min = Number.MAX_VALUE;
-    var max = Number.MIN_VALUE;
+function numbersWar(numbers) {
+    var oddScore = 0;
+    var evenScore = 0;
     for (var i = 0; i < numbers.length; i++) {
-        if (numbers[i] < min)
-            min = numbers[i];
-        if (numbers[i] > max)
-            max = numbers[i];
+        if (numbers[i] % 2 == 0)
+            evenScore += numbers[i];
+        else
+            oddScore += numbers[i];
     }
-    console.log(min);
-    console.log(max);
-    return max - min;
+    console.log("score of odd number is " + oddScore);
+    console.log("score of even number is " + evenScore);
+    if (evenScore > oddScore)
+        return "even numbers won";
+    else if (oddScore > evenScore)
+        return "odd numbers won";
+    else
+        return "strength of both sides is same";
 }
-console.log(minMaxDifference([10, 2, 3, 5, 9, -4, -5]));
+console.log(numbersWar([5, 1, 2, 5, 7, 8, 4, 7, 9, 1, 2, 4, 7, 8, 6, 4]));
 //# sourceMappingURL=helloappslab.js.map
